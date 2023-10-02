9f2034e9770bf39531ddceec8fb15616aa3d79de
HMS <48788641+ItsHMS@users.noreply.github.com>
2023-10-02 15:35:15 -0700

    send response after few seconds

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -3,7 +3,9 @@ import { Router } from "express";
 const routes = Router();
 
 routes.get("/", async (req, res) => {
-  res.status(200).send({ message: "Hello World!" });
+  setTimeout(() => {
+    res.status(200).send({ message: "Hello World!" });
+  }, 3200);
 });
 
 export default routes;
