15012efcd5898f8c1af61aab7522b6d7eec134a5
HMS <48788641+ItsHMS@users.noreply.github.com>
2023-10-02 15:44:31 -0700

    35seconds wait

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -4,8 +4,8 @@ const routes = Router();
 
 routes.get("/", async (req, res) => {
   setTimeout(() => {
-    res.status(200).send({ message: "Hello World!" });
-  }, 3200);
+    res.status(200).send({ message: "Hello!" });
+  }, 35000);
 });
 
 export default routes;
