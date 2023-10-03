fb24fe584ae63038a9d08a4d6756beef9aa495be
HMS <48788641+ItsHMS@users.noreply.github.com>
2023-10-03 01:08:58 -0700

    updates with same path but different methods

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -5,7 +5,12 @@ const routes = Router();
 routes.get("/", async (req, res) => {
   setTimeout(() => {
     res.status(200).send({ message: "Hello!" });
-  }, 35000);
+  }, 3000);
+});
+routes.post("/", async (req, res) => {
+  setTimeout(() => {
+    res.status(200).send({ message: "Hello!" });
+  }, 3000);
 });
 
 export default routes;
