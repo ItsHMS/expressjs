1d57e70fe022c7ea13f7a9b676be7beeacf09c87
HMS <48788641+ItsHMS@users.noreply.github.com>
2023-10-03 01:48:44 -0700

    updates

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import fs from 'fs'
 
 const routes = Router();
+const path = '../users.json'
 
+const readData = () => {
+  const data = fs.readFileSync(path);
+  return JSON.parse(data);
+};
 routes.get("/", async (req, res) => {
   setTimeout(() => {
-    res.status(200).send({ message: "Hello!" });
+    let data  = readData()
+    res.status(200).send({ message: data });
   }, 3000);
 });
 routes.post("/", async (req, res) => {
